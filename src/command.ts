4a96a8bdfb16b9b03df3bbfb12f8b36d5dbf1158
BreathlessWay <731005087@qq.com>
2020-09-28 17:13:59 +0800

    feat: commander demo

--- src/command.ts
+++ src/command.ts
@@ -7,10 +7,19 @@ const packageJson = require("../package.json");
 const cli: InterfaceCLI = new Command();
 
 cli
+  .description("Generate Mini Program Template")
   .version(packageJson.version)
   .option("-d, --debug", "show debug info")
-  .option("-p, --project-name <name>", "current project name")
-  .parse(process.argv)
-  .help();
+  .requiredOption("-p, --project-name <name>", "current project name")
+  .action((options: InterfaceCLI) => {
+    console.log(options.projectName);
+  })
+  .on("--help", () => {
+    console.log("");
+    console.log("Example call:");
+    console.log("  $ mini --help");
+    console.log("");
+  })
+  .parse(process.argv);
 
 export default cli;
