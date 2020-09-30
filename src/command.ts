ed93ad5d277334b1de264e2031b82e874b35c74f
BreathlessWay <731005087@qq.com>
2020-09-30 16:41:22 +0800

    chore: husky

--- src/command.ts
+++ src/command.ts
@@ -9,17 +9,17 @@ const packageJson = require("../package.json");
 
 const cli: InterfaceCLI = new Command();
 
-cli
-  .description("Generate Mini Program Template").version(packageJson.version, "-v, --version")
-  .option("-d, --debug", "show debug info")
-  .option("-i, --init <name>", "init project name")
-  .action((option: InterfaceCLI) => {
-    handleOptions(option);
-  })
-  .on("--help", () => {
-    log.log("");
-    log.log("Example call:");
-    log.log("  $ mini --help");
-    log.log("");
-  })
-  .parse(process.argv);
+cli.description("Generate Mini Program Template")
+    .version(packageJson.version, "-v, --version")
+    .option("-d, --debug", "show debug info")
+    .option("-i, --init <name>", "init project name")
+    .action((option: InterfaceCLI) => {
+        handleOptions(option);
+    })
+    .on("--help", () => {
+        log.log("");
+        log.log("Example call:");
+        log.log("  $ mini --help");
+        log.log("");
+    })
+    .parse(process.argv);
